2944fd20de88b71b054bb7e6338c1f79700ba869
ephemeralfuture <48807244+ephemeralfuture@users.noreply.github.com>
2021-07-31 12:33:34 +0100

    Add files via upload
    The Arduino code for reading and printing RFID UID tags

---
+++ detached_UID.ino
@@ -0,0 +1,43 @@
+//Most code from https://randomnerdtutorials.com/security-access-using-mfrc522-rfid-reader-with-arduino/
+
+#include <SPI.h>
+#include <MFRC522.h>
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+ 
+void setup() 
+{
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+}
+void loop() 
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  { Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  content.toUpperCase();
+  /*if (content.substring(1) == "C3 45 33 0B") //change here the UID of the card/cards that you want to give access
+  {
+    delay(500);
+  }*/
+
+} 
