6329e939505af0ee4970298419af93cbcc63922c
ephemeralfuture <48807244+ephemeralfuture@users.noreply.github.com>
2021-08-01 22:58:49 +0100

    Add files via upload

--- detached_UID.ino
+++ detached_UID.ino
@@ -1,4 +1,6 @@
 //Most code from https://randomnerdtutorials.com/security-access-using-mfrc522-rfid-reader-with-arduino/
+//this program runs an RFID reader and then runs a short function to convert this into a string that easily identifies different cards
+//for use in a Python program on Raspberry Pi
 
 #include <SPI.h>
 #include <MFRC522.h>
@@ -12,34 +14,37 @@ void setup()
   Serial.begin(9600);   // Initiate a serial communication
   SPI.begin();      // Initiate  SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC522
+  //Serial.println();
 }
 void loop() 
 {
   // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) 
   {
+    Serial.println("not read");
+    delay(100);
     return;
   }
   // Select one of the cards
   if ( ! mfrc522.PICC_ReadCardSerial()) 
   {
+    Serial.println("not read");
+    delay(100);
     return;
   }
   //Show UID on serial monitor
+  Serial.print("UID tag :");
   String content= "";
   byte letter;
   for (byte i = 0; i < mfrc522.uid.size; i++) 
-  { Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+  { 
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
      Serial.print(mfrc522.uid.uidByte[i], HEX);
      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
      content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
   content.toUpperCase();
   Serial.println();
-  delay(3000);
-  /*if (content.substring(1) == "C3 45 33 0B") //change here the UID of the card/cards that you want to give access
-  {
-    delay(500);
-  }*/
+  delay(1000);
 
 } 
